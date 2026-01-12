ed7e32b11e6844b88f7302533a65a4867ad49f62
kittulal <punnakrishnaa@gmail.com>
2026-01-12 05:59:15 +0530

    first commit

---
+++ src/main/java/com/krish/spring_tx_mgmt/SpringTxMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.krish.spring_tx_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTxMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTxMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krish/spring_tx_mgmt/SpringTxMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krish.spring_tx_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
